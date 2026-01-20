9003e54eca3dde48fe6fb1f74bb233c7b073a211
Richard Croker <ra.croker@gmail.com>
2026-01-20 11:58:41 +0000

    Revert "fix deciles chart toggle"
    This reverts commit 55dc882218439195adae81b862865ceb9b321b3a.

--- app/open_pathology.py
+++ app/open_pathology.py
@@ -47,8 +47,7 @@ def main():
     with streamlit.expander("Caveats"):
         streamlit.markdown(measure.caveats)
 
-    chart = measure.deciles_chart(cb_safe=cb_safe)
-    streamlit.altair_chart(chart, use_container_width=True)
+    streamlit.altair_chart(measure.deciles_chart, cb_safe=cb_safe, use_container_width=True)
 
     streamlit.subheader("Demographic breakdowns")
 
