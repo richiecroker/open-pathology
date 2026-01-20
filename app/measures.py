846942e2ff877ef26ce24d13969c5fe095029f52
Richard Croker <ra.croker@gmail.com>
2026-01-20 14:16:03 +0000

    Revert "update get to cb mode"
    This reverts commit 214f741e2e228519b5d0d35d082618bf4e7327ea.

--- app/measures.py
+++ app/measures.py
@@ -123,13 +123,12 @@ class OSJobsRepository:
         self._records = {r["name"]: r for r in yaml.load(path.read_text(), yaml.Loader)}
         self._measures = {}  # the repository
 
-    def get(self, name, color_blind_mode=False):  # <- Add parameter here
+    def get(self, name):
         """Get the measure with the given name from the repository."""
         log.info(f'Getting "{name}" from the repository')
-        cache_key = (name, color_blind_mode)  # <- NEW LINE
-        if cache_key not in self._measures:  # <- Changed from 'name' to 'cache_key'
-            self._measures[cache_key] = self._construct(name, color_blind_mode)  # <- Add parameter
-        return self._measures[cache_key]  # <- Changed from 'name' to 'cache_key'
+        if name not in self._measures:
+            self._measures[name] = self._construct(name)
+        return self._measures[name]
 
     def _construct(self, name):
         """Construct the measure with the given name from information stored on the
