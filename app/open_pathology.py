7d36e4441acf77ee89c850c11f68e46b784f8ef6
Richard Croker <ra.croker@gmail.com>
2026-01-20 14:15:47 +0000

    Revert "Update open_pathology.py"
    This reverts commit 07a7571415010e88cbb7c716179041f12a8b194e.

--- app/open_pathology.py
+++ app/open_pathology.py
@@ -4,6 +4,8 @@ import pandas as pd
 
 streamlit.set_page_config(layout="wide")
 
+color_blind_mode = streamlit.checkbox('Colour-blind friendly palette')
+
 streamlit.markdown(
     """
     <style>
@@ -19,45 +21,53 @@ streamlit.markdown(
 def get_repository():
     return measures.OSJobsRepository()
 
+
 def main():
     repository = get_repository()
-    
+
     streamlit.title("OpenPathology")
-    
-    color_blind_mode = streamlit.checkbox('Colour-blind friendly palette')  # Move it here
-    
+
     selected_measure_name = streamlit.selectbox("Select a measure:", repository.list())
-    measure = repository.get(selected_measure_name, color_blind_mode=color_blind_mode)  # Pass the parameter
-    
+
+    measure = repository.get(selected_measure_name)
+
     streamlit.header(measure.name)
+
     streamlit.markdown(
         "The codes used for this measure "
         f"are available in [this codelist]({measure.codelist_url})."
     )
-    
+
     with streamlit.expander("What is it and why does it matter?"):
         streamlit.markdown(measure.explanation)
-    
+
     with streamlit.expander("Design"):
         streamlit.markdown(measure.design)
-    
+
     with streamlit.expander("Caveats"):
         streamlit.markdown(measure.caveats)
-    
+
     streamlit.altair_chart(measure.deciles_chart, use_container_width=True)
-    
+
     streamlit.subheader("Demographic breakdowns")
+
     if measure.measures_tables:
         selected_demographic = streamlit.selectbox(
             "Select a demographic breakdown:", sorted(measure.measures_tables.keys())
         )
+
         streamlit.altair_chart(measure.measure_chart(selected_demographic))
+
     else:
         streamlit.markdown("No demographic breakdowns are available.")
-    
+
     streamlit.subheader(f"Most common codes ([codelist]({measure.codelist_url}))")
+
     streamlit.dataframe(measure.top_5_codes_table)
+
     streamlit.markdown(f"Total events: {measure.total_events:,} events")
+
+
 
 if __name__ == "__main__":
     main()
